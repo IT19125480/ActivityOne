be242cb24a815a81ede5722684ad4e1efa4de81a
thili <Thilinik835@gmail.com>
2020-08-08 09:35:34 +0530

    Second Commit

---
+++ app/src/main/java/com/example/newactivityone/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.newactivityone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView2;
+
+    private static final String TAG = "MainActivity";
+    
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView2 = findViewById(R.id.textView2);
+        textView2.setText(R.string.Msg02);
+
+        Log.i(TAG, "onCreate: ");
+    }
+}
